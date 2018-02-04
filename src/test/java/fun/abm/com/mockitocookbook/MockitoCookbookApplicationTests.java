1508d4825f3b3889a186ccd25eef318af18d06da
nelsondavenapalli <nelson.csits@gmail.com>
2018-02-05 00:00:34 +0100

    Initial commit

---
+++ src/test/java/fun/abm/com/mockitocookbook/MockitoCookbookApplicationTests.java
@@ -0,0 +1,16 @@
+package fun.abm.com.mockitocookbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockitoCookbookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
